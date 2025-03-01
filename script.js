5d1f69add677011af734922f0a3a1cd26a3d5513
Aravind Manivannan <192905623+arav-mani21@users.noreply.github.com>
2025-02-28 22:09:48 -0500

    updated index.html updated script.js file to produce calculator buttons upon DOM loading updated script.js file to run function that prints numbers and operations in display text when pressing respective buttons

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+let buttons = "123+456-789*0./C ="
+
+document.addEventListener("DOMContentLoaded",loadCalculator())
+
+function loadCalculator(){
+    const containerDiv = document.querySelector('#container')
+    for(const char of buttons){
+        const buttonItem = document.createElement("button")
+
+        buttonItem.textContent = char
+
+        if(char == '0' || char == 'C'){
+            buttonItem.classList.toggle('double')
+        }
+        else if (char == ' '){
+            buttonItem.classList.toggle('space')
+        }
+        else{
+            buttonItem.classList.toggle('single')
+        }
+        buttonItem.addEventListener('click',display(char))
+        containerDiv.appendChild(buttonItem)
+    }
+}
+
+function display(c){
+    const calcDisplay = document.querySelector(".display")
+    let displayText = calcDisplay.textContent
+    let operations = ['+','-','*','/']
+
+    operations = operations.filter(char => displayText.includes(char))
+
+    if(operations.length != 0){
+        let indexOfOperand = displayText.indexOf(operations[0])
+        const firstNum = +displayText.slice(0,indexOfOperand)
+        const secondNum = +displayText.slice(indexOfOperand+1)
+
+        let result = operate(operations[0],firstNum,secondNum)
+        calcDisplay.textContent = result
+    }
+    calcDisplay.textContent += c
+}
+
 function operate(operation,a,b){
     if (operation == '+')
         return add(a,b)
